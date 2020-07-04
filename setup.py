6392ac173be139e8318f2c61fe5cc4ba8f238057
swfarnsworth <swfarnsworth@gmail.com>
2020-07-04 11:51:20 -0400

    Added setup, need to test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='madlibert',
+    version='0.0.0',
+    description='Fill in the blank with BERT',
+    author='Steele Farnsworth',
+    install_requires=[
+        'torch',
+        'transformers'
+    ]
+)
