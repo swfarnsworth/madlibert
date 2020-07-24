bf88de42d656a879e2bf2f87796e906a08ad61a1
swfarnsworth <swfarnsworth@gmail.com>
2020-07-24 13:54:17 -0400

    Fixed setup.py so it installs the module and not just its dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 setup(
     name='madlibert',
+    packages=find_packages(),
     version='0.0.0',
     description='Fill in the blank with BERT',
     author='Steele Farnsworth',
